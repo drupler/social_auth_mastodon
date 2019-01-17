587ce0196e1abbba63911de07b55b73c132c089d
Getulio Sánchez <valentin2507@gmail.com>
2019-01-17 15:25:03 -0500

    Issue #3026714: Get callback URL from route

--- src/Plugin/Network/MastodonAuth.php
+++ src/Plugin/Network/MastodonAuth.php
@@ -5,14 +5,13 @@ namespace Drupal\social_auth_mastodon\Plugin\Network;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Logger\LoggerChannelFactoryInterface;
-use Drupal\Core\Routing\RequestContext;
-use Drupal\social_auth\SocialAuthDataHandler;
+use Drupal\Core\Site\Settings;
+use Drupal\Core\Url;
 use Drupal\social_api\Plugin\NetworkBase;
 use Drupal\social_api\SocialApiException;
 use Drupal\social_auth_mastodon\Settings\MastodonAuthSettings;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Lrf141\OAuth2\Client\Provider\Mastodon;
-use Drupal\Core\Site\Settings;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Defines a Network Plugin for Social Auth Mastodon.
@@ -34,25 +33,11 @@ use Drupal\Core\Site\Settings;
 class MastodonAuth extends NetworkBase implements MastodonAuthInterface {
 
   /**
-   * The Social Auth Data Handler.
-   *
-   * @var \Drupal\social_auth\SocialAuthDataHandler
-   */
-  protected $dataHandler;
-
-  /**
    * The logger factory.
    *
    * @var \Drupal\Core\Logger\LoggerChannelFactory
    */
   protected $loggerFactory;
-
-  /**
-   * The request context object.
-   *
-   * @var \Drupal\Core\Routing\RequestContext
-   */
-  protected $requestContext;
 
   /**
    * The site settings.
@@ -64,20 +49,14 @@ class MastodonAuth extends NetworkBase implements MastodonAuthInterface {
   /**
    * {@inheritdoc}
    */
-  public static function create(
-      ContainerInterface $container,
-      array $configuration,
-      $plugin_id,
-      $plugin_definition) {
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     return new static(
-      $container->get('social_auth.data_handler'),
       $configuration,
       $plugin_id,
       $plugin_definition,
       $container->get('entity_type.manager'),
       $container->get('config.factory'),
       $container->get('logger.factory'),
-      $container->get('router.request_context'),
       $container->get('settings')
     );
   }
@@ -85,8 +64,6 @@ class MastodonAuth extends NetworkBase implements MastodonAuthInterface {
   /**
    * MastodonAuth constructor.
    *
-   * @param \Drupal\social_auth\SocialAuthDataHandler $data_handler
-   *   The data handler.
    * @param array $configuration
    *   A configuration array containing information about the plugin instance.
    * @param string $plugin_id
@@ -99,34 +76,27 @@ class MastodonAuth extends NetworkBase implements MastodonAuthInterface {
    *   The configuration factory object.
    * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
    *   The logger factory.
-   * @param \Drupal\Core\Routing\RequestContext $requestContext
-   *   The Request Context Object.
    * @param \Drupal\Core\Site\Settings $settings
-   *   The settings factory.
+   *   The site settings.
    */
-  public function __construct(SocialAuthDataHandler $data_handler,
-                              array $configuration,
+  public function __construct(array $configuration,
                               $plugin_id,
                               array $plugin_definition,
                               EntityTypeManagerInterface $entity_type_manager,
                               ConfigFactoryInterface $config_factory,
                               LoggerChannelFactoryInterface $logger_factory,
-                              RequestContext $requestContext,
-                              Settings $settings
-  ) {
+                              Settings $settings) {
 
     parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager, $config_factory);
 
-    $this->dataHandler = $data_handler;
     $this->loggerFactory = $logger_factory;
-    $this->requestContext = $requestContext;
     $this->siteSettings = $settings;
   }
 
   /**
    * Sets the underlying SDK library.
    *
-   * @return Lrf141\OAuth2\Client\Provider\Mastodon|false
+   * @return \Lrf141\OAuth2\Client\Provider\Mastodon|false
    *   The initialized 3rd party library instance.
    *   False if could not be initialized.
    *
@@ -139,6 +109,7 @@ class MastodonAuth extends NetworkBase implements MastodonAuthInterface {
     if (!class_exists($class_name)) {
       throw new SocialApiException(sprintf('The Mastodon Library for the OAuth2 not found. Class: %s.', $class_name));
     }
+
     /* @var \Drupal\social_auth_mastodon\Settings\MastodonAuthSettings $settings */
     $settings = $this->settings;
 
@@ -147,7 +118,7 @@ class MastodonAuth extends NetworkBase implements MastodonAuthInterface {
       $league_settings = [
         'clientId' => $settings->getClientId(),
         'clientSecret' => $settings->getClientSecret(),
-        'redirectUri' => $this->requestContext->getCompleteBaseUrl() . '/user/login/mastodon/callback',
+        'redirectUri' => Url::fromRoute('social_auth_mastodon.callback')->setAbsolute()->toString(),
         'instance' => $settings->getInstance(),
         'scope' => 'read:accounts',
       ];
@@ -162,6 +133,7 @@ class MastodonAuth extends NetworkBase implements MastodonAuthInterface {
 
       return new Mastodon($league_settings);
     }
+
     return FALSE;
   }
 
