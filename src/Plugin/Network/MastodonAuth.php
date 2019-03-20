76db9b8af323aaf22a88cb9b5eb680dbedc4fbeb
Getulio Sánchez <valentin2507@gmail.com>
2019-03-20 02:05:07 -0400

    Issue #3041669: Use Social Auth Network Base

--- src/Plugin/Network/MastodonAuth.php
+++ src/Plugin/Network/MastodonAuth.php
@@ -2,16 +2,11 @@
 
 namespace Drupal\social_auth_mastodon\Plugin\Network;
 
-use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Logger\LoggerChannelFactoryInterface;
-use Drupal\Core\Site\Settings;
 use Drupal\Core\Url;
-use Drupal\social_api\Plugin\NetworkBase;
 use Drupal\social_api\SocialApiException;
+use Drupal\social_auth\Plugin\Network\NetworkBase;
 use Drupal\social_auth_mastodon\Settings\MastodonAuthSettings;
 use Lrf141\OAuth2\Client\Provider\Mastodon;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Defines a Network Plugin for Social Auth Mastodon.
@@ -31,68 +26,6 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * )
  */
 class MastodonAuth extends NetworkBase implements MastodonAuthInterface {
-
-  /**
-   * The logger factory.
-   *
-   * @var \Drupal\Core\Logger\LoggerChannelFactory
-   */
-  protected $loggerFactory;
-
-  /**
-   * The site settings.
-   *
-   * @var \Drupal\Core\Site\Settings
-   */
-  protected $siteSettings;
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('entity_type.manager'),
-      $container->get('config.factory'),
-      $container->get('logger.factory'),
-      $container->get('settings')
-    );
-  }
-
-  /**
-   * MastodonAuth constructor.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param array $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager.
-   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
-   *   The configuration factory object.
-   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
-   *   The logger factory.
-   * @param \Drupal\Core\Site\Settings $settings
-   *   The site settings.
-   */
-  public function __construct(array $configuration,
-                              $plugin_id,
-                              array $plugin_definition,
-                              EntityTypeManagerInterface $entity_type_manager,
-                              ConfigFactoryInterface $config_factory,
-                              LoggerChannelFactoryInterface $logger_factory,
-                              Settings $settings) {
-
-    parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager, $config_factory);
-
-    $this->loggerFactory = $logger_factory;
-    $this->siteSettings = $settings;
-  }
-
   /**
    * Sets the underlying SDK library.
    *
@@ -120,7 +53,6 @@ class MastodonAuth extends NetworkBase implements MastodonAuthInterface {
         'clientSecret' => $settings->getClientSecret(),
         'redirectUri' => Url::fromRoute('social_auth_mastodon.callback')->setAbsolute()->toString(),
         'instance' => $settings->getInstance(),
-        'scope' => 'read:accounts',
       ];
 
       // Proxy configuration data for outward proxy.
@@ -154,6 +86,7 @@ class MastodonAuth extends NetworkBase implements MastodonAuthInterface {
       $this->loggerFactory
         ->get('social_auth_mastodon')
         ->error('Define Client ID and Client Secret on module settings.');
+
       return FALSE;
     }
 
--- src/Plugin/Network/MastodonAuthInterface.php
+++ src/Plugin/Network/MastodonAuthInterface.php
@@ -2,7 +2,7 @@
 
 namespace Drupal\social_auth_mastodon\Plugin\Network;
 
-use Drupal\social_api\Plugin\NetworkInterface;
+use Drupal\social_auth\Plugin\Network\NetworkInterface;
 
 /**
  * Defines the Mastodon Auth interface.
